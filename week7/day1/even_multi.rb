473027975601d639fe98ac24378df0ed649773d9
Pouyan Madanipour <pouyan.mad@gmail.com>
2020-02-03 10:18:37 +1100

    updates

---
+++ week7/day1/even_multi.rb
@@ -0,0 +1,22 @@
+
+# numbers = [14, 5, 7, 2, 50, 25, 10, -4, 3, 3, 25, 2, 6, 5]
+
+# numbers.each do |num|
+#     if num.even? and num % 5 == 0
+#     puts num
+#     end
+# end
+
+def even_multiples numbers
+
+    result = []
+    numbers.each do |num|
+        if num.even? and num % 5 == 0
+            result << num
+        end
+    end
+    result
+end
+
+p even_multiples [14, 5, 7, 2, 50, 25, 10, -4, 3, 3, 25, 2, 6, 5]
+p even_multiples [500, 50, 25, 10, 5]
---
+++ week7/day1/even_multiples.md
@@ -0,0 +1,5 @@
+# Even Multiples
+
+Write a method called `even_multiples` that takes a `list_of_numbers` and returns only numbers that are **even** AND **multiples of 5**.
+
+For example the `list_of_numbers` could be `[14, 5, 7, 2, 25, 10, -4, 3, 3, 25, 2, 6, 5]`.
