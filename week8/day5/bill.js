6e3a53a0e9f0d48afd717a7d3efb9436ca4f46bd
Pouyan Madanipour <pouyan.mad@gmail.com>
2020-02-15 21:09:21 +1100

    updates

--- week8/day5/bill.js
+++ week8/day5/bill.js
@@ -2,24 +2,24 @@
 let group = {
     A: 20, 
     B: 15, 
-    C: 10
+    C: 10,
+    D: 40
 }
 
 // const entries = Object.values(entries)
 
-let CalculateBill = function (object) {
+let calculateBill = function (object) {
 
     const names = Object.keys(object)
     const share = Object.values(object)
     
-    let reduce = share.reduce((total, money) => money - total)
-    console.log(reduce)
+    let reduce = share.reduce((total, money) => money  - total)
+    console.log(reduce);
 
     let bill = {}
-    
     
     names.forEach((name, index) => bill[name] = share[index] - reduce)
 
     return bill
 }
-console.log(CalculateBill(group))
+console.log(calculateBill(group))
